c4c13682a6adb6f6a1229cc834e9e1f3ce48dfc1
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2024-08-10 05:04:15 -0300

    Update testnpprint.py

--- testnpprint.py
+++ testnpprint.py
@@ -22,11 +22,3 @@ for d in dtypes:
     tmp = c.astype(d)
     numpyzigprint(tmp)
 
-# a2 = np.arange(10 * 20 * 3).reshape((10, 20, 3)).astype(np.uint64)
-# numpyzigprint(a2)
-
-# a3 = np.arange(2 * 4 * 3 * 2 * 5).reshape((2, 4, 3, 2, 5)).astype(np.uint64)
-# numpyzigprint(a3)
-# fl2 = a3.ravel()
-# fl2shape = np.array(a3.shape, dtype=np.uint64)
-# zignumpyprint.npprint.numpy2zig(fl2, a3.dtype.char, fl2shape)
